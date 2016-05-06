63b73f1b9fe164c881d5c1841c9ec1ef22eb73af
Andres Felipe Bernal <Andres Felipe Bernal@172.25.9.92>
2016-05-06 11:44:56 -0500

    ya estan completa la gestion del equipo

---
+++ Quiz/src/java/VO/Aerolina.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VO;
+
+/**
+ *
+ * @author Andres Felipe Bernal
+ */
+public class Aerolina {
+    public String nombreAerolina;
+
+    public String getNombreAerolina() {
+        return nombreAerolina;
+    }
+
+    public void setNombreAerolina(String nombreAerolina) {
+        this.nombreAerolina = nombreAerolina;
+    }
+    
+}
